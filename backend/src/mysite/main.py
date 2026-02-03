c6a3930aaf426205819ebfea77415915ae010925
abdullahabdelaziz122 <abdullah.abdelaziz15@gmail.com>
2026-02-04 01:30:16 +0200

    Changed the index for Backend connection, Client connected successfully to backend

--- backend/src/mysite/main.py
+++ backend/src/mysite/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Simple Todo API")
 # Enable CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:8000", "http://localhost:3000"],
+    allow_origins=["http://localhost:5000", "http://127.0.0.1:5500", "http://localhost:8000", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
